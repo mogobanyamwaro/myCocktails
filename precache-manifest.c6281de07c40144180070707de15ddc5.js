b0d7a414f834585aee727cebb0318b9542382718
mogobanyamwaro <mogobanyamwaro@gmail.com>
2021-07-26 15:33:10 +0300

    Updates

---
+++ precache-manifest.c6281de07c40144180070707de15ddc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e920978455985db4fbc6745c51e0141e",
+    "url": "/myCocktails/index.html"
+  },
+  {
+    "revision": "29c3e12d818593e6b726",
+    "url": "/myCocktails/static/css/main.28c0c0e6.chunk.css"
+  },
+  {
+    "revision": "e1f6309a6f0d3e350015",
+    "url": "/myCocktails/static/js/2.72303b8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/myCocktails/static/js/2.72303b8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29c3e12d818593e6b726",
+    "url": "/myCocktails/static/js/main.7e6c732d.chunk.js"
+  },
+  {
+    "revision": "d6be20b385399363c40b",
+    "url": "/myCocktails/static/js/runtime-main.37cc6ce6.js"
+  },
+  {
+    "revision": "9a3d26451b294fc390aee4ae147a45c7",
+    "url": "/myCocktails/static/media/logo.9a3d2645.svg"
+  }
+]);
